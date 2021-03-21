d7b9e9624ba695b22423938df4070657d5538fad
tsjrw <tesexbox360@hotmail.com>
2021-03-21 21:01:07 +0000

    Conexion cliente base datos cambiada

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,7 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const cliente = new Client({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 cliente.connect((err => {
